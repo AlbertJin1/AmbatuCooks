9fb91004cb2609af2e592226d454f57bac5bac69
Yotsuchi <awsomegamingracerhd3@gmail.com>
2023-04-04 16:43:29 +0800

    1.3

--- Finalv1/assets/script.js
+++ Finalv1/assets/script.js
@@ -16,3 +16,54 @@ $('.home-carousel').owlCarousel({
         }
     }
 })
+
+// NAVBAR
+window.addEventListener('scroll', function(){
+    let navbar = document.getElementById("navbar");
+    navbar.classList.toggle('fixed', this.window.scrollY > 0)
+})
+
+// NAV BUTTONS
+let menuBtn = document.querySelector('.menu-btn');
+let searchBtn = document.querySelector('.searchbtn');
+let cartBtn = document.querySelector('.cartbtn');
+let darkBtn = document.querySelector('.darkbtn');
+
+searchBtn.onclick = function(){
+    document.getElementById("search-form").classList.toggle('active');
+
+    if(document.getElementById("search-form").classList.contains('active')){
+        searchBtn.classList.remove("bx-search-alt-2");
+        searchBtn.classList.add("bx-x");
+    }
+    else{
+        searchBtn.classList.remove("bx-x");
+        searchBtn.classList.add("bx-search-alt-2");
+    }
+}
+
+cartBtn.onclick = function(){
+    document.getElementById("cart").classList.toggle('active');
+
+    if(document.getElementById("cart").classList.contains('active')){
+        cartBtn.classList.remove("bx-cart");
+        cartBtn.classList.add("bx-x");
+    }
+    else{
+        cartBtn.classList.remove("bx-x");
+        cartBtn.classList.add("bx-cart");
+    }
+}
+
+darkBtn.onclick = function(){
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        darkBtn.classList.remove("bx-moon");
+        darkBtn.classList.add("bx-sun");
+    }
+    else{
+        darkBtn.classList.remove("bx-sun");
+        darkBtn.classList.add("bx-moon");
+    }
+}
