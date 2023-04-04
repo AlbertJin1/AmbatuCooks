3b367b68d6a18f9da48fb0b3028ed3112ce9f144
Yotsuchi <awsomegamingracerhd3@gmail.com>
2023-04-04 21:34:57 +0800

    1.6

--- Finalv1/assets/script.js
+++ Finalv1/assets/script.js
@@ -1,24 +1,24 @@
 // HOME CARO
 $('.home-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
+    loop:true,
+    margin:0,
     dots: true,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
+    nav:false,
+    responsive:{
+        0:{
+            items:1
         },
-        600: {
-            items: 1
+        600:{
+            items:1
         },
-        1000: {
-            items: 1
+        1000:{
+            items:1
         }
     }
 })
 
 // NAVBAR
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function(){
     let navbar = document.getElementById("navbar");
     navbar.classList.toggle('fixed', this.window.scrollY > 0)
 })
@@ -30,42 +30,42 @@ let cartBtn = document.querySelector('.cartbtn');
 let darkBtn = document.querySelector('.darkbtn');
 let signBtn = document.getElementById('signImg');
 
-searchBtn.onclick = function () {
+searchBtn.onclick = function(){
     document.getElementById("search-form").classList.toggle('active');
 
-    if (document.getElementById("search-form").classList.contains('active')) {
+    if(document.getElementById("search-form").classList.contains('active')){
         searchBtn.classList.remove("bx-search-alt-2");
         searchBtn.classList.add("bx-x");
     }
-    else {
+    else{
         searchBtn.classList.remove("bx-x");
         searchBtn.classList.add("bx-search-alt-2");
     }
 }
 
-cartBtn.onclick = function () {
+cartBtn.onclick = function(){
     document.getElementById("cart").classList.toggle('active');
 
-    if (document.getElementById("cart").classList.contains('active')) {
+    if(document.getElementById("cart").classList.contains('active')){
         cartBtn.classList.remove("bx-cart");
         cartBtn.classList.add("bx-x");
     }
-    else {
+    else{
         cartBtn.classList.remove("bx-x");
         cartBtn.classList.add("bx-cart");
     }
 }
 
-darkBtn.onclick = function () {
+darkBtn.onclick = function(){
     document.body.classList.toggle('dark-mode');
 
-    if (document.body.classList.contains('dark-mode')) {
+    if(document.body.classList.contains('dark-mode')){
         darkBtn.classList.remove("bx-moon");
         darkBtn.classList.add("bx-sun");
 
         signImg.src = './img/sign/sign-dark.png';
     }
-    else {
+    else{
         darkBtn.classList.remove("bx-sun");
         darkBtn.classList.add("bx-moon");
 
@@ -75,22 +75,24 @@ darkBtn.onclick = function () {
 
 // MENU SECTION
 let menuTabs = document.querySelector('.menu-tabs');
-menuTabs.addEventListener('click', function (e) {
-    if (e.target.classList.contains('menu-tab-item') && !e.target.contains('active')) {
+
+menuTabs.addEventListener('click', function(e){
+
+    if(e.target.classList.contains('menu-tab-item') && !e.target.classList.contains('active')){
 
         const target = e.target.getAttribute("data-target");
 
-        menuTabs.querySelector('active').classList.remove('active');
+        menuTabs.querySelector('.active').classList.remove('active');
 
         e.target.classList.add("active");
 
         let menuSection = document.querySelector(".menu-section");
 
         menuSection.querySelector(".menu-tab-content.show").classList.remove("show");
-
         menuSection.querySelector(target).classList.add("show");
     }
-    else {
+
+    else{
         return
     }
 })
