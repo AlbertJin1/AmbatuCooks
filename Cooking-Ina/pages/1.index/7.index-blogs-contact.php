f8bc4be9077f0d0dc533d0060dcd58a44fbd2045
jEn1ry <johnhenleyllamos8@gmail.com>
2024-04-18 23:23:08 +0800

    blog update

--- Cooking-Ina/pages/1.index/7.index-blogs-contact.php
+++ Cooking-Ina/pages/1.index/7.index-blogs-contact.php
@@ -4,8 +4,9 @@
 
     <div class="section-heading" class="menu-tab-content show" data-aos="zoom-in" data-aos-duration="1000">
         <h3>Our Recipe</h3>
-        <h1>Certified Ambatupicks</h1>
+        <h1>To Recomend</h1>
         <div class="square-wrapper flex-row">
+            <div class="square"></div>
             <div class="square"></div>
             <div class="square"></div>
             <div class="square"></div>
@@ -13,6 +14,83 @@
     </div>
 
     <div class="blog-carousel owl-carousel owl-theme" data-aos="fade-right" data-aos-duration="1000">
+
+        <!-- BLOG POST START -->
+        <a href="recipes-list.php#popup1-main" class="blog-post">
+
+            <div class="img-wrapper">
+                <img class="lazy" data-src="./img/Blog/lumpia.png" alt="#">
+                <div class="overlay"><i class="icon bx bx-link"></i></div>
+            </div>
+
+            <div class="text-wrapper">
+                <h5 class="blog-title">Lumpiang Shanghai</h5>
+                <p class="desc">Lumpiang Shanghai is a Filipino deep-fried appetizer consisting of a mixture of giniling with vegetables like carrots, chopped scallions or red onions and garlic, wrapped in a thin egg crêpe. </p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>1 hr</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>20 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>1 hr 20 mins</span></p>
+            </div>
+
+        </a>
+        <!-- BLOG POST END -->
+
+        <!-- BLOG POST START -->
+        <a href="recipes-list.php#popup1-appe" class="blog-post">
+
+            <div class="img-wrapper">
+                <img class="lazy" data-src="./img/Blog/buko.png" alt="#">
+                <div class="overlay"><i class="icon bx bx-link"></i></div>
+            </div>
+
+            <div class="text-wrapper">
+                <h5 class="blog-title">Buko Pandan</h5>
+                <p class="desc">Buko Pandan is a popular Filipino Dessert made using young coconut,  pandan leaves (or Screwpine leaves), and sago pearls. At first glance, this dessert dish can be mistaken for Buko Salad. Both desserts are almost similar visually.</p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>10 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>10 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>20 mins</span></p>
+            </div>
+
+        </a>
+        <!-- BLOG POST END -->
+
+        <!-- BLOG POST START -->
+        <a href="recipes-list.php#popup3-dessert" class="blog-post">
+
+            <div class="img-wrapper">
+                <img class="lazy" data-src="./img/Blog/mango.png" alt="#">
+                <div class="overlay"><i class="icon bx bx-link"></i></div>
+            </div>
+
+            <div class="text-wrapper">
+                <h5 class="blog-title">Mango Juice</h5>
+                <p class="desc">The heat of these summer days is horrible. We need all the drinks we can get on a hot day and, to me, the best drinks are homemade juice.</p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span> 10 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Making Time: <span>5 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>15 mins</span></p>
+            </div>
+
+        </a>
+        <!-- BLOG POST END -->
+
+        <!-- BLOG POST START -->
+        <a href="recipes-list.php#popup2-beve" class="blog-post">
+
+            <div class="img-wrapper">
+                <img class="lazy" data-src="./img/Blog/ube.png" alt="#">
+                <div class="overlay"><i class="icon bx bx-link"></i></div>
+            </div>
+
+            <div class="text-wrapper">
+                <h5 class="blog-title">Ube Cheese Pandesal</h5>
+                <p class="desc">Soft, fluffy, and delicious - this easy Ube Cheese Pandesal recipe will become a favorite breakfast or snack recipe! It's the classic Filipino pandesal with a sweet ube flavor, melted cheese filling, and bread crumbs coating. Yum!</p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>23 mins</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>15 mins</span></p>
+                \<p class="time"><i class="icon bx bx-time-five"></i>Resting Time: <span>2 hrs</span></p>
+                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>2 hrs 38 mins</span></p>
+            </div>
+
+        </a>
+        <!-- BLOG POST END -->
 
         <!-- BLOG POST START -->
         <a href="recipes-list.php#popup1-main" class="blog-post">
@@ -28,63 +106,6 @@
                 <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>10 mins</span></p>
                 <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>15 mins</span></p>
                 <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>25 mins</span></p>
-            </div>
-
-        </a>
-        <!-- BLOG POST END -->
-
-        <!-- BLOG POST START -->
-        <a href="recipes-list.php#popup1-appe" class="blog-post">
-
-            <div class="img-wrapper">
-                <img class="lazy" data-src="./img/Blog/2-Chawanmushi.jpg" alt="#">
-                <div class="overlay"><i class="icon bx bx-link"></i></div>
-            </div>
-
-            <div class="text-wrapper">
-                <h5 class="blog-title">Chawanmushi 茶碗蒸し</h5>
-                <p class="desc">Chawanmushi is a classic Japanese savory custard that`s steamed and served in a delicate cup.</p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>20 mins</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>30 mins</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>50 mins</span></p>
-            </div>
-
-        </a>
-        <!-- BLOG POST END -->
-
-        <!-- BLOG POST START -->
-        <a href="recipes-list.php#popup3-dessert" class="blog-post">
-
-            <div class="img-wrapper">
-                <img class="lazy" data-src="./img/Blog/3-Japanese Strawberry Shortcake.jpg" alt="#">
-                <div class="overlay"><i class="icon bx bx-link"></i></div>
-            </div>
-
-            <div class="text-wrapper">
-                <h5 class="blog-title">Japanese Strawberry Shortcake いちごのショートケーキ</h5>
-                <p class="desc">This simple and elegant cake is perfect for celebrating all occasions.</p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>2 hours</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>25 mins</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>2 hours 25 mins</span></p>
-            </div>
-
-        </a>
-        <!-- BLOG POST END -->
-
-        <!-- BLOG POST START -->
-        <a href="recipes-list.php#popup2-beve" class="blog-post">
-
-            <div class="img-wrapper">
-                <img class="lazy" data-src="./img/Blog/4-Matcha Latte.jpg" alt="#">
-                <div class="overlay"><i class="icon bx bx-link"></i></div>
-            </div>
-
-            <div class="text-wrapper">
-                <h5 class="blog-title">Matcha Latte 抹茶ラテ</h5>
-                <p class="desc">This hot beverage with green tea powder is packed with antioxidants and tastes better than Starbucks.</p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Prep Time: <span>2 mins</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Cook Time: <span>3 mins</span></p>
-                <p class="time"><i class="icon bx bx-time-five"></i>Total Time: <span>5 mins</span></p>
             </div>
 
         </a>
